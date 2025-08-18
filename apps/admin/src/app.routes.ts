1fb234778b3320dd972a2be023bc4da47ebd25b3
Salih YILDIZ <yldzsalih27@gmail.com>
2025-08-18 11:01:42 +0300

    For product routes refactoring

--- apps/admin/src/app.routes.ts
+++ apps/admin/src/app.routes.ts
@@ -13,17 +13,9 @@ export const appRoutes: Route[] = [
         path: '',
         loadComponent: () => import('./pages/home/home'),
       },
-       {
+      {
         path: 'products',
-        loadComponent: () => import('./pages/products/products'),
-      },
-       {
-        path: 'products/create',
-        loadComponent: () => import('./pages/products/create/create'),
-      },
-       {
-        path: 'products/edit/:id',
-        loadComponent: () => import('./pages/products/create/create'),
+        loadChildren: () => import('./pages/products/routes')
       }
     ]
   }
