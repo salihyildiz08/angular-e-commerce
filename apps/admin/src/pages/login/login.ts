0e025c83aabb92c47cb9f1703e74a2a78b252b38
Salih YILDIZ <yldzsalih27@gmail.com>
2025-08-22 09:37:49 +0300

    Login page

--- apps/admin/src/pages/login/login.ts
+++ apps/admin/src/pages/login/login.ts
@@ -1,11 +1,32 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectionStrategy, Component, inject, ViewEncapsulation } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { UserModel } from '../users/users';
+import { FlexiToastService } from 'flexi-toast';
+import { Router } from '@angular/router';
+
+
 
 @Component({
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export default class Login {
+readonly #http=inject(HttpClient);
+readonly #toast=inject(FlexiToastService);
+readonly #router=inject(Router);
 
+  signIn(form:NgForm){
+    if(!form.valid) return;
+    this.#http.get<UserModel[]>(`api/users?username=${form.value['userName']}&password=${form.value['password']}`).subscribe((res)=>{
+      if(res.length===0){
+       this.#toast.showToast("Error","Invalid username or password","error");
+       return;
+      }
+      localStorage.setItem("response",JSON.stringify(res[0]));
+      this.#router.navigateByUrl("/");
+    });
+  }
 }
