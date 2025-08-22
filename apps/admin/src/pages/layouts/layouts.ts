308eece9fc89ea6da58b898b0bcb0b38d4941774
Salih YILDIZ <yldzsalih27@gmail.com>
2025-08-22 10:32:58 +0300

    Placing logged in user information in the sidebar and navbar

--- apps/admin/src/pages/layouts/layouts.ts
+++ apps/admin/src/pages/layouts/layouts.ts
@@ -1,10 +1,24 @@
-import { ChangeDetectionStrategy, Component, computed, signal, ViewEncapsulation } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  linkedSignal,
+  signal,
+  ViewEncapsulation,
+} from '@angular/core';
 import Breadcrumb from './breadcrumb';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import {
+  Router,
+  RouterLink,
+  RouterLinkActive,
+  RouterOutlet,
+} from '@angular/router';
 import { navigations } from '../../navigation';
 import { NavPipe } from '../../pipes/nav-pipe';
 import { FormsModule } from '@angular/forms';
 import { DatePipe } from '@angular/common';
+import { Common } from '../../services/common';
 
 @Component({
   imports: [
@@ -14,21 +28,30 @@ import { DatePipe } from '@angular/common';
     NavPipe,
     FormsModule,
     DatePipe,
-    RouterOutlet
+    RouterOutlet,
   ],
   templateUrl: './layouts.html',
   encapsulation: ViewEncapsulation.None,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class Layouts {
   readonly search = signal<string>('');
   readonly time = signal<Date>(new Date());
   readonly navigations = computed(() => navigations);
+  readonly user = computed(() => this.#common.user()!);
+
+  readonly #router = inject(Router);
+
+  readonly #common = inject(Common);
 
   constructor() {
     setInterval(() => {
       this.time.set(new Date());
-    },1000)
-}
-}
+    }, 1000);
+  }
 
+ logout(){
+    localStorage.clear();
+    this.#router.navigateByUrl("/login");
+  }
+}
