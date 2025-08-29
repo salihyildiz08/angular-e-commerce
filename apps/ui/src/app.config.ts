fb8fdd70af633c7d556cfe3b64b7a3be6a30ee19
Salih YILDIZ <yldzsalih27@gmail.com>
2025-08-29 10:14:38 +0300

    Creating layouts and home components

---
+++ apps/ui/src/app.config.ts
@@ -0,0 +1,18 @@
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+  provideZonelessChangeDetection,
+} from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { appRoutes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+provideZonelessChangeDetection(),
+provideHttpClient(),
+    provideRouter(appRoutes),
+  ],
+};
