af397620ad3af2e25b957a3a6403e23e2d4d1a02
Salih YILDIZ <yldzsalih27@gmail.com>
2025-09-26 10:05:32 +0300

    Increasing or decreasing the number of items in the basket

--- apps/ui/src/pages/baskets/baskets.ts
+++ apps/ui/src/pages/baskets/baskets.ts
@@ -1,8 +1,9 @@
-import { httpResource } from '@angular/common/http';
+import { HttpClient, httpResource } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component, computed, inject, ViewEncapsulation } from '@angular/core';
 import { BasketModel } from '@shared/models/basket.model';
 import { Common } from '../../services/common';
 import { TrCurrencyPipe } from 'tr-currency';
+import { FlexiToastService } from 'flexi-toast';
 
 @Component({
   imports: [TrCurrencyPipe],
@@ -28,4 +29,32 @@ readonly result = httpResource<BasketModel[]>(() => {
   readonly kdv = computed(() => this.total() * 18 / 100);
 
   readonly #common = inject(Common);
+readonly #http = inject(HttpClient);
+  readonly #toast = inject(FlexiToastService);
+
+  increment(val: BasketModel){
+    val.quantity++;
+    this.#http.put(`api/baskets/${val.id}`,val).subscribe(() => {
+      this.result.reload();
+    });
+  }
+
+  decrement(val: BasketModel){
+    const count = val.quantity - 1;
+
+    if(count <= 0){
+      this.#toast.showSwal("Sil?","Ürünü sepetten silmek istiyor musunuz?","Sil",() => {
+        this.#http.delete(`api/baskets/${val.id}`).subscribe(() => {
+          this.result.reload();
+        });
+      })
+    }else{
+      val.quantity--;
+      this.#http.put(`api/baskets/${val.id}`,val).subscribe(() => {
+        this.result.reload();
+      });
+    }
+  }
+
+
 }
